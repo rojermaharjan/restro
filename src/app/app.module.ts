55cbdc98cd09e0c587df0a7195ce37eb05b77f06
rojermn <rojermn@gmail.com>
2020-10-10 08:44:41 +0545

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { HeaderFixedDirective } from './directives/header-fixed.directive';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderFixedDirective,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // 3rd Party
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
