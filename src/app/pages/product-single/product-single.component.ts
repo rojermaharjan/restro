10eef00984b0076ab439aa80bfbf393cc032d03b
rojermn <rojermn@gmail.com>
2020-10-15 16:09:57 +0545

    product detail wip

--- src/app/pages/product-single/product-single.component.ts
+++ src/app/pages/product-single/product-single.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-single.component.scss']
 })
 export class ProductSingleComponent implements OnInit {
+  productQty = 1;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  updateQty(isPlus: boolean): void {
+    if (isPlus) {
+      this.productQty += 1;
+    } else
+      if (this.productQty !== 1) {
+        this.productQty -= 1;
+      }
+  }
 }
+
