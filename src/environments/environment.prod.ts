bf8bd2f6474c38c6486e770e8646761bd6ada779
immortal infidel <immortal.infidel@gmail.com>
2021-01-09 10:54:59 +0545

    pdf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://mmadmin.yodea.org/',
+  api_url: 'http://mmadmin.yodea.org',
   auth_end: 'http://localhost:3001/api/auth/self',
 };
